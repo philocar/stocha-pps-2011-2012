81eb73360ee771a181bef3b550c520b284638411
fabien.bini@gmail.com <fabien.bini@gmail.com@8b435bec-9705-11c8-cfca-5a630600017c>
2011-11-19 16:02:44 +0000

    Ajout de la classe PreparationMatlab qui permet de créer un fichier contenant un programme cvx résolvant le problème et qui peut être utilisé dans Matlab.

---
+++ trunk/src/manager/PreparationMatlab.java
@@ -0,0 +1,21 @@
+package manager;
+
+import data.Data;
+
+/**
+ * Classe permettant la création d'un fichier exécutable par Matlab pour la résolution probabiliste du problème.
+ * 
+ * @author Fabien BINI & Nathanaël MASRI & Nicolas POIRIER
+ *
+ */
+public class PreparationMatlab {
+	
+	/**
+	 * Génère un fichier .m utilisable dans Matlab sous la forme d'un programme cvx.
+	 * @param donnees Les données du problème
+	 * @param fichier Le fichier d'écriture
+	 */
+	public static void generer(Data donnees, String fichier){
+		
+	}
+}
