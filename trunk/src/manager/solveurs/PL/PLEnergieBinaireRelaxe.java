95234fb54e77f0365e3176eba49bd4b4f99f140b
groupeCMT07@gmail.com <groupeCMT07@gmail.com@8b435bec-9705-11c8-cfca-5a630600017c>
2011-12-12 17:59:07 +0000

    - CPLEX binaire relaxé

---
+++ trunk/src/manager/solveurs/PL/PLEnergieBinaireRelaxe.java
@@ -0,0 +1,64 @@
+package manager.solveurs.PL;
+
+import data.DataBinaire;
+import data.solution.SolutionEnergieBinaireRelaxe;
+
+public abstract class PLEnergieBinaireRelaxe {
+	/** Variable contenant les données du problème */
+	protected DataBinaire donnees;
+	/** Le vecteur de solution du problème */
+	protected SolutionEnergieBinaireRelaxe solution;
+	/** Le vecteur de coûts */
+	protected double[] couts;
+	/** Le gain avec les apports en eau */
+	protected double gains;
+	/** Les probabilités des scénarios */
+	protected double[] probabilites;
+
+	/**
+	 * Crée un nouveau PLEnergie.
+	 * 
+	 * @param donnees
+	 *            les données du problème
+	 */
+	public PLEnergieBinaireRelaxe(DataBinaire donnees) {
+		this.donnees = donnees;
+		solution = new SolutionEnergieBinaireRelaxe(donnees);
+		genererPL();
+	}
+
+	/**
+	 * Génère le programme linéaire à partir des données. Remplit les tableaux
+	 * du problème.
+	 */
+	private void genererPL() {
+		int nbPaliers = donnees.nbPaliers[0] + donnees.nbPaliers[1] + donnees.nbPaliers[2] + donnees.nbPaliers[3];
+		couts = new double[donnees.nbPeriodes * nbPaliers + donnees.nbTrajectoires];
+		gains = 0;
+		for (int p = 0; p < donnees.nbPeriodes; p++) {
+			int sommePaliers = 0;
+			for (int c = 0; c < donnees.nbCentrales; c++) {
+				for (int numPalier = 0; numPalier < donnees.nbPaliers[c]; numPalier++) {
+					couts[p * nbPaliers + sommePaliers] = donnees.getCoutCentrale(c) * donnees.getPalier(c, numPalier);
+					sommePaliers++;
+				}
+			}
+			gains += donnees.getApportsPeriode(p) *  donnees.getCoutCentrale(4);
+		}
+		for (int i = 0; i < donnees.nbTrajectoires; i++) {
+			couts[donnees.nbPeriodes * nbPaliers + i] = donnees.nbPeriodes * donnees.getCoutCentrale(4)
+					* donnees.getTrajectoire(i) / donnees.getTurbinage();
+		}
+	}
+
+	/**
+	 * Renvoie la solution calculée.
+	 * 
+	 * @return la solution calculée
+	 */
+	public SolutionEnergieBinaireRelaxe getSolution() {
+		return solution;
+	}
+
+	public abstract void lancer();
+}
